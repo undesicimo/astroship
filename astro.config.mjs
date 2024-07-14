a6ff75008fe27a16f60a0fb15cd3bac830e6c52a
undesicimo <105863111+undesicimo@users.noreply.github.com>
2024-07-14 19:13:20 +0900

    todos

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  // TODO
+  site: "",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
